17c827528c1b952bbf75b53047c76116c7839bd3
dsodx <dsodx@pm.me>
2023-06-03 09:28:19 +0300

    Create main script

---
+++ bot/__main__.py
@@ -0,0 +1,24 @@
+import logging
+import asyncio
+
+from aiogram import Bot, Dispatcher
+
+logger = logging.getLogger(name=__name__)
+
+
+async def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+
+    bot = Bot(token=...)
+    dp = Dispatcher()
+
+    logger.warning(msg="Starting bot...")
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logger.warning(msg="Bot stopped")
