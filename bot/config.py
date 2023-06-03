1f5aea9484ef38de6595612b69d68742a0424c7a
dsodx <dsodx@pm.me>
2023-06-03 20:47:48 +0300

    Create db settings

--- bot/config.py
+++ bot/config.py
@@ -1,12 +1,19 @@
-from pydantic import BaseSettings, SecretStr
+from pydantic import BaseSettings, BaseModel, SecretStr, PostgresDsn
+
+
+class Postgres(BaseModel):
+    dsn: PostgresDsn
 
 
 class Settings(BaseSettings):
     bot_token: SecretStr
     owner_id: int
 
+    postgres: Postgres
+
     class Config:
         env_file = ".env"
+        env_nested_delimiter = "_"
 
 
 config = Settings()
