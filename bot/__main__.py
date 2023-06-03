6a0285a84268a4662e344b91aabe29904b3f5789
dsodx <dsodx@pm.me>
2023-06-03 09:49:45 +0300

    Update main script

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
 
 from .config import config
 from .handlers import setup_routers
@@ -19,8 +20,9 @@ async def on_startup(bot: Bot, dp: Dispatcher) -> None:
 async def main() -> None:
     logging.basicConfig(level=logging.INFO)
 
-    bot = Bot(token=config.bot_token.get_secret_value())
+    bot = Bot(token=config.bot_token.get_secret_value(), parse_mode=ParseMode.HTML)
     dp = Dispatcher()
+    dp["config"] = config
 
     await on_startup(bot=bot, dp=dp)
 
